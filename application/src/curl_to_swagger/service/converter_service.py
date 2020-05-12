62a748bec56f60cfd776930425c8dad53fbe6486
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2020-05-12 14:14:20 +0200

    Improved import of ConverterService in Resource

--- application/src/curl_to_swagger/service/converter_service.py
+++ application/src/curl_to_swagger/service/converter_service.py
@@ -1,6 +1,6 @@
 import logging
 
-from curl_to_swagger_core.model.model import PayloadModel, CurlModel
+from curl_to_swagger_core.model.model import PayloadModel, CurlModel, SwaggerModel
 from curl_to_swagger_core.services import ConverterService
 
 logger = logging.getLogger(f'c2s.{__name__}')
@@ -12,4 +12,7 @@ class ConverterServiceImpl(ConverterService):
         pass
 
     def convert_to_curl(self, payload_model: PayloadModel) -> CurlModel:
+        pass
+
+    def convert_to_swagger(self, curl_model: CurlModel) -> SwaggerModel:
         pass
--- application/src/curl_to_swagger_api/resource/curltoswagger_resources.py
+++ application/src/curl_to_swagger_api/resource/curltoswagger_resources.py
@@ -1,9 +1,10 @@
 import json
 
-from curl_to_swagger_core.service import ConverterService
-from flask import Response
+from flask import request, Response
 from flask_restful import Resource
 from injector import inject
+
+from curl_to_swagger_core.services import ConverterService
 
 
 class CurlToSwaggerResource(Resource):
@@ -13,5 +14,6 @@ class CurlToSwaggerResource(Resource):
         self._converter_service = converter_service
 
     def post(self):
+        data = request.get_json()
         response = json.dumps({'id': 'helloworld'})
         return Response(status=201, response=response, mimetype='application/json')
